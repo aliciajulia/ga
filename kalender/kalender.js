c731ceef5f5819354227808e2e2bad22916b616e
Johan Kellén <johan.kellen@gmail.com>
2015-03-27 17:16:44 +0100

    kalender typ klar
    Det saknas fortfarande funktion att byta månad men den gör jag på
    söndag. Ni kan nu använda dessa filer. Kolla i filerna hur de används
    eller döp om min kalender.html och bygg vidare i den istället.

---
+++ kalender/kalender.js
@@ -0,0 +1,103 @@
+//$(this).siblings("input").val();
+$(document).ready(main);
+
+function main() {
+    
+    var nextMonth = "";
+    var prevMonth = "";
+    initListeners();
+    getDatum();
+}
+
+function initListeners() {
+    $('.btnNext').click(function () {
+        console.log('nextMonth klickad');
+        $('#kalender').children().remove();
+        console.log(nextMonth);
+        getDatum(nextMonth);
+    });//click button
+}
+
+function addDateListener() {
+    $('.bookableDay').click(function () {
+        console.log("dag klickad");
+        getTider($(this));
+    });//click bookable
+}
+
+function addTimeListener() {
+    $('.bookableTime').click(function () {
+        console.log("tid klickad");
+        $('#dag').slideToggle();
+        $('#bokningsform').slideToggle();
+        $('#starttid').val($(this).attr("data-date"));
+        $('#selectedDate').text($('#starttid').val().substr(0, 10));
+        $('#selectedTime').text($('#starttid').val().substr(11, 5));
+    });//click bookable
+}
+
+function getTider(e) {
+    var starttid = $(e).text();
+    $('#kalender').slideToggle();
+
+    $.getJSON("getTider.php", {starttid: starttid})
+            .done(function (data) {
+                console.log(data);
+                var tmp_html = "";
+                $.each(data, function (key, value) {
+                    console.log(key + ", " + value.starttid);
+                    tmp_html = tmp_html + "<li class='bookableTime' data-date='" + value.starttid + "'>" + value.starttid + "</li>";
+                });//.each
+                tmp_html = "<ul>" + tmp_html + "</ul>";
+                $('#dag').append(tmp_html);
+                $('#dag').slideToggle();
+                addTimeListener();
+            });//done + getJSON
+}//getDatum
+
+function getDatum(date) {
+    $.getJSON("getDatum.php", {date: date})
+            .done(function (data) {
+                var tmp_html = "";
+                $.each(data, function (key, value) {
+                    tmp_html = tmp_html + "<li class='" + value.class + "'>" + value.starttid + "</li>";
+                    //kolla slut av vecka och skriv till veckan och resetta
+                    if (key % 7 == 6) {
+                        tmp_html = "<ul>" + tmp_html + "</ul>";
+                        $('#kalender').append(tmp_html);
+                        tmp_html = "";
+                    }
+                });//.each
+                tmp_html = "<ul>" + tmp_html + "</ul>";
+                $('#kalender').append(tmp_html);
+                addDateListener();
+                nextMonth = data[0]["nextMonth"];
+                prevMonth = data[0]["prevMonth"];
+            });//done + getJSON
+}//getDatum
+
+
+//function addBookingListeners() {
+//    $("form").submit(function (event) {
+//        console.log("submit fångad, förbereder getJSON");
+//        event.preventDefault();
+//
+//
+////        console.log(selectedDate);
+//
+//
+//
+//        var starttid = $('#starttid').val();
+//        var kundNamn = $('#kundNamn').val();
+//        var kundTelefon = $('#kundTelefon').val();
+//        var kundMail = $('#kundMail').val();
+//
+//
+//        $.getJSON("bokaTid.php", {starttid: selectedDate, kundNamn: kundNamn, kundTelefon: kundTelefon, kundMail: kundMail})
+//                .done(function (data) {
+//                    console.log(data);
+//                });//done + getJSON
+//
+//    });//submit
+//
+//}
